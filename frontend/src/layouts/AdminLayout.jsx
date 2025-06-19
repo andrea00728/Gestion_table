186818f1732181fcb59af31d9ec57ae8ac921706
andrea00728 <andrea112samuel@gmail.com>
2025-06-19 14:53:44 +0200

    configuration routeur

---
+++ frontend/src/layouts/AdminLayout.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function <Admin></Admin>Layout(){
+    return (
+        <>
+        <div><Admin></Admin>Layout</div>
+        </>
+    )
+}
---
+++ frontend/src/routers/router.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { createBrowserRouter, Navigate, Router } from "react-router-dom";
+import DefaultLayout from "../layouts/DefaultLayout";
+import GuestLayout from "../layouts/GuestLayout";
+import Accueil from "../pages/Accueil";
+import Evenement from "../pages/Evenement";
+import Apropos from "../pages/Apropos";
+import Notfound from "../pages/Notofoundpage";
+import Pageconnexion from "../pages/Pageconnexion";
+import AdminLayout from "../layouts/AdminLayout";
+import Connexionorganisateur from "../pages/Connexionorganisateur";
+const router=createBrowserRouter([
+    {
+        path:"/",
+        element:<DefaultLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<Navigate to="/accueil"/>
+            },{
+                path:"/accueil",
+                element:<Accueil/>
+            },
+            {
+                path:"/evenement",
+                element:<Evenement/>
+            },
+            {
+                path:"/apropos",
+                element:<Apropos/>
+            }
+        ]
+    },
+     {
+        path:"/",
+        element:<AdminLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<Navigate to="/AdminAccueil"/>
+            },{
+                path:"/AdminAccueil",
+                element:<Adminaccueil/>
+            },
+        ]
+    },
+    {
+        path:"/",
+        element:<GuestLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<Navigate to="/pageconnexion" />
+            },
+            {
+                path:"/pageconnexion",
+                element:<Pageconnexion/>
+            },
+            {
+                path:"/connexion",
+                element:<Connexionorganisateur/>
+            },
+            {
+                path:"/inscription",
+                element:<Inscription/>
+            }
+        ]
+    },
+    {
+        path:"*",
+        element:<Notfound/>
+    }
+    
+]);
+
+export default router;
